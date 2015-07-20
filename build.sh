0645af3bd654d0b6a5039b44ff4e03e8e8b41c73
Lev Givon <lev@columbia.edu>
2015-07-20 10:19:01 -0400

    Enable support for termcap, termlib, and wide chars.

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ mkdir $PREFIX/lib
 
 sh ./configure --prefix=$PREFIX \
     --without-debug --without-ada --without-manpages \
-    --with-shared --disable-overwrite
-
+    --with-shared --disable-overwrite --enable-termcap \
+    --with-termlib --enable-widec
 
 make -j$(getconf _NPROCESSORS_ONLN)
 make install
